7c6c08bfb2fae0601f0d18dd043942b9d90aa0d8
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-03-12 13:59:11 +0000

    introduced configuration insertion for worker connectors & cleardown existing configs before reinserting

--- main.py
+++ main.py
@@ -21,17 +21,38 @@ def get_parameters():
         description="Configure Confluent Kafka Consumers using RESTful API")
 
     # Parse command line inputs and set defaults
+    parser.add_argument('--connector.name', default='s3-sink')
     parser.add_argument('--aws-profile', default='default')
     parser.add_argument('--aws-region', default='eu-west-2')
+    parser.add_argument('--tasks-max', default='1')
+    parser.add_argument('--topics', default='')
+    parser.add_argument('--flush-size', default='1')
+    parser.add_argument('--s3-bucket-name', default='')
 
     _args = parser.parse_args()
 
     # Override arguments with environment variables where set
+    if 'CONNECTOR_NAME' in os.environ:
+        _args.aws_profile = os.environ['CONNECTOR_NAME']
+
     if 'AWS_PROFILE' in os.environ:
         _args.aws_profile = os.environ['AWS_PROFILE']
 
     if 'AWS_REGION' in os.environ:
         _args.aws_region = os.environ['AWS_REGION']
+
+    if 'TASKS_MAX' in os.environ:
+        _args.tasks_max = os.environ['TASKS_MAX']
+
+    if 'TOPICS' in os.environ:
+            _args.tasks_max = os.environ['TOPICS']
+
+    if 'FLUSH_SIZE' in os.environ:
+        _args.tasks_max = os.environ['FLUSH_SIZE']
+
+    if 'S3_BUCKET_NAME' in os.environ:
+        _args.tasks_max = os.environ['S3_BUCKET_NAME']
+
 
     return _args
 
@@ -59,6 +80,50 @@ def configure_confluent_kafka_consumer(event, args):
     message = json.loads(event['Records'][0]['Sns']['Message'])
     logger.debug(message)
 
+    private_ip = message['detail']['containers'][0]['networkInterfaces'][0]['privateIpv4Address']
+    logger.debug(private_ip)
+
+    payload = {
+        "config": {
+            "connector.name": "s3-sink",
+            "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+            "tasks.max": args.tasks_max,
+            "topics": args.topics,
+            "flush.size": args.flush_size,
+            "s3.region": args.aws_region,
+            "s3.bucket.name": args.s3_bucket_name,
+            "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+            "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+            "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+            "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+        }
+    }
+    response = requests.get(f"http://{private_ip}:8083/connectors")
+    existing_connector = response.content.decode("utf-8")
+    logger.debug(existing_connector)
+    print (existing_connector)
+
+    # existing_connector = ['s3-sink', 'other-connector']
+
+    if existing_connector in existing_connector:
+        logger.debug("update connector [PUT]")
+
+        response = requests.put(f"http://{private_ip}:8083/connectors", json=payload)
+        logger.debug(response.content.decode("utf-8"))
+
+    else:
+
+        logger.debug("create connector [POST]")
+
+        response = requests.post(f"http://{private_ip}:8083/connectors", json=payload)
+        logger.debug(response.content.decode("utf-8"))
+
+    for existing_connector in existing_connector:
+        if existing_connector not in args.topics:
+            logger.debug("delete connector [DELETE]")
+
+            response = requests.post(f"http://{private_ip}:8083/connectors", json=payload)
+            logger.debug(response.content.decode("utf-8"))
 
 if __name__ == "__main__":
     try:
@@ -68,3 +133,4 @@ if __name__ == "__main__":
         logger.error("Unexpected error occurred")
         logger.error(e)
         raise e
+
